4f76e0001b2581269854ad72ad0b6c3af92a5f10
Matthew J. Gerber <47827960+gerber211@users.noreply.github.com>
2022-10-04 11:39:26 +0900

    Update notes.py

--- notes.py
+++ notes.py
@@ -6,3 +6,19 @@ def dist_to_origin(point):
 # get simple timestamp 
 from datetime import datetime
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_")
+
+# Calculate difference between neighboring xyz points
+dd = np.diff(traj_xyzs[0:3, :], axis=1)
+# Euclidean distances between each
+segdists = np.sqrt((dd ** 2).sum(axis=0))
+
+# Make axes of 3D plot to have equal scales... matplotlib's methods don't work
+def set_axes_equal_3d(ax):
+    limits = np.asarray([ax.get_xlim3d(), ax.get_ylim3d(), ax.get_zlim3d()])
+    spans = abs(limits[:, 0] - limits[:, 1])
+    centers = np.mean(limits, axis=1)
+    radius = 0.5 * max(spans)
+    ax.set_xlim3d([centers[0]-radius, centers[0]+radius])
+    ax.set_ylim3d([centers[1]-radius, centers[1]+radius])
+    ax.set_zlim3d([centers[2]-radius, centers[2]+radius])
+
