ccceee49e2e39ea6437fd75ddd292990a0e341bc
Matthew J. Gerber <47827960+gerber211@users.noreply.github.com>
2022-10-04 09:59:48 +0900

    Update notes.py
    added timestamp

--- notes.py
+++ notes.py
@@ -1,4 +1,8 @@
-@staticmethod
+# vector length, 2D or 3D 
 def dist_to_origin(point):
     # calculate Euclidean distance; agnostic towards vector length
     return np.sqrt((np.asarray(point) ** 2).sum(axis=0))
+
+# get simple timestamp 
+from datetime import datetime
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_")
